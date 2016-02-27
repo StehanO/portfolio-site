8b084f85bd3ca52deae2b9ee73e24f25fe4d293f
Francois Laubscher <francoislaubscher@outlook.com>
2016-02-27 11:26:41 +0200

    Rebuilt everything and its so much better now

---
+++ js/site.js
@@ -0,0 +1,189 @@
+var projects = [
+    {
+        title: "Mobile Itinerary template",
+        date: "2016",
+        employer: "wetu",
+        employerlink: "http://wetu.com",
+        description: "Mobile device specific Itinerary template built as a Single Page Application using JavaScript.",
+        role: "Development of Mobile Itinerary template.",
+        tech: [
+            ".Net Framework 4.5.2",
+            "MS SQL Server 2014",
+            "RavenDB",            
+            "ASP.NET MVC 5",
+            "SCSS",
+            "JavaScript"
+        ]        
+    },
+    {
+        title: "Specials portal",
+        date: "2015",
+        employer: "wetu",
+        employerlink: "http://wetu.com",
+        description: "Allow clients to create special offers linked to their Accommodations. Clients can search for specials created using the specials search portal.",
+        role: "Development of specials creator and specials search portal.",
+        tech: [
+            ".Net Framework 4.5.2",
+            "MS SQL Server 2014",
+            "RavenDB",            
+            "ASP.NET MVC 5",
+            "SCSS"            
+        ]        
+    },
+    {
+        title: "Metrics dashboard",
+        date: "August 2015",
+        employer: "wetu",
+        employerlink: "http://wetu.com",
+        description: "Internal metrics dashboard",
+        role: "Developed metrics dashboard using d3.js",
+        tech: [
+            ".Net Framework 4.5.2",
+            "MS SQL Server 2014",
+            "RavenDB",            
+            "ASP.NET MVC 5",
+            "d3.js",
+            "SCSS",
+            "JavaScript"
+        ]        
+    },
+    {
+        title: "Hybrid Image management",
+        date: "2015",
+        employer: "wetu",
+        employerlink: "http://wetu.com",
+        description: "Allow clients to upload their own images for Destinations / Accommodations. Multi-lingual image descriptions can be added to an image and selected images can be used for print outputs.",
+        role: "Developed hybrid image management portal.",
+        tech: [
+            ".Net Framework 4.5.2",
+            "MS SQL Server 2014",
+            "RavenDB",
+            "ASP.NET MVC 5",
+            "SCSS",
+            "JavaScript"
+        ]        
+    },
+    {
+        title: "Multi-lingual directions",
+        date: "2015",
+        employer: "wetu",
+        employerlink: "http://wetu.com",
+        description: "Allow clients to add multi-lingual directions to or from their Accommodations.",
+        role: "Developed multi-lingual directions editor. Directions are generated using Google maps API and translated based on language selection.",
+        tech: [
+            ".Net Framework 4.5.2",
+            "MS SQL Server 2014",
+            "RavenDB",
+            "ASP.NET MVC 5",
+            "SCSS",
+            "JavaScript"
+        ]        
+    },
+    {
+        title: "Citizen Reporting (FixMyCity framework v2)",
+        date: "March 2015",
+        employer: "Intervate",
+        employerlink: "http://intervate.com",
+        description: "The Fix-My-City Framework is a reusable app framework which utilises the common components of a smartphone. These components include the global positioning system (GPS), the 3G or Wi-Fi receiver, and the camera.",
+        role: "Development of Windows Phone app, SQL Azure database, ASP.NET web application and WCF REST web services.",
+        tech: [
+            "Windows Azure",
+            "WCF REST web services",
+            ".Net Framework 4.5",
+            "SQL Azure",
+            "Windows Phone 8",
+            "ASP.NET Web Forms",
+        ]        
+    },
+    {
+        title: "JRA Find & Fix",
+        date: "February 2014 to March 2015",
+        employer: "Intervate",
+        employerlink: "http://intervate.com",
+        description: "The purpose of the Fix & Find App is to simplify and enhance communication between Johannesburg Road Agency (JRA) and the road users, to enable the JRA to process the service requests of citizens, to resolve road issues and to enable road users to share road issues.",
+        role: "Development of Windows Phone app, SQL Azure database, ASP.NET web application and WCF REST web services.",
+        tech: [
+            "Windows Azure",
+            "WCF REST web services",
+            ".Net Framework 4.5",
+            "SQL Azure",
+            "Windows Phone 8",
+            "ASP.NET Web Forms",
+            "d3.js"
+        ]        
+    },
+    {
+        title: "SANParks App v2",
+        date: "October 2013 to January 2014",
+        employer: "Intervate",
+        employerlink: "http://intervate.com",
+        description: "The SANParks app enables individuals to capture animal sightings during game drives. It provides the ability to capture details about sightings along with a snapshot to keep a visual history of your trip and to assist in tracking of endangered animals.",
+        role: "Development of Windows Phone app, SQL Azure database, Windows Azure Mobile services and ASP.NET web application.",
+        tech: [
+            "Windows Azure",
+            "Windows Azure Mobile Services",
+            ".Net Framework 4.5",
+            "SQL Azure",
+            "Windows Phone 8",
+            "ASP.NET Web Forms"
+        ]        
+    },
+    {
+        title: "Telkom Knockout App",
+        date: "October 2013",
+        employer: "Intervate",
+        employerlink: "http://intervate.com",
+        description: "Telkom Knockout Tournament app enables users to stay up to date with all the Telkom Knockout action and news. The tournament features the 16 PSL teams playing in straight knockout ties through a round of the last 16, quarterfinals and semi-finals, before the final is played at a neutral ground.",
+        role: "Development of Windows Phone app",
+        tech: [
+            "HTML5, CSS3 and JavaScript",
+            "Windows Phone 8",
+        ]        
+    },
+    {
+        title: "Citizen Scientist",
+        date: "June 2013",
+        employer: "Intervate",
+        employerlink: "http://intervate.com",
+        description: "The Citizen Scientist app enables individuals to capture animal sightings during game drives. It provides the ability to capture details about sightings along with a snapshot to keep a visual history of your trip and to assist in tracking of endangered animals.",
+        role: "Development of Windows Phone and Windows apps, SQL Azure database, Windows Azure Mobile services.",
+        tech: [
+            "Windows Azure",
+            "Windows Azure Mobile Services",
+            ".Net Framework 4.5",
+            "SQL Azure",
+            "Windows Phone 8",
+            "Windows 8"
+        ]        
+    },
+    {
+        title: "Road Accident Fund - Accident Report",
+        date: "May 2013",
+        employer: "Intervate",
+        employerlink: "http://intervate.com",
+        description: "The RAF application is designed to assist you in the event of an accident. It provides a quick-reference guide as to what to do in the event of an accident or emergency, access to emergency numbers and a convenient way to capture the key information required from the scene of an accident for reference or later reporting to the police, insurance or to the Road Accident Fund.",
+        role: "Development of Windows Phone app, SQL Azure database, Windows Azure Mobile services.",
+        tech: [
+            "Windows Azure",
+            "Windows Azure Mobile Services",
+            ".Net Framework 4.5",
+            "SQL Azure",
+            "Windows Phone 8"
+        ]        
+    },
+    {
+        title: "FixMyCity Framework",
+        date: "March 2013",
+        employer: "Intervate",
+        employerlink: "http://intervate.com",
+        description: "The Fix-My-City Framework is a reusable app framework which utilises the common components of a smartphone. These components include the global positioning system (GPS), the 3G or Wi-Fi receiver, and the camera.",
+        role: "Development of Windows Phone app, SQL Azure database, ASP.Net web backend and web services.",
+        tech: [
+            "Windows Azure",
+            "ASP.Net Web Forms",
+            ".Net Framework 4.5",
+            "SQL Azure",
+            "Windows Phone 8"
+        ]        
+    }
+];
