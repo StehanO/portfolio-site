d052a6d4288d5e0b3268ea0f060f602280943e27
Francois Laubscher <francoislaubscher@outlook.com>
2016-10-21 16:44:02 +0200

    Updated projects and display date

--- views/index.jsx
+++ views/index.jsx
@@ -5,8 +5,8 @@ export const Home = () =>
         <head>
             <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0"/>
             <meta name="author" content="Francois Laubscher"/>
-            <meta name="description" content="Hi! I'm Francois. For the past few years I've worked as a full stack developer, primarily developing web apps using ASP.NET. I've recently started focusing my free time and career on developing within the JavaScript stack using tools such NodeJS, React and Redux."/>
-            <meta name="keywords" content="Francois,Laubscher,JavaScript,JS,NodeJS,React,Cape Town,South Africa,Developer,Web developer"/>
+            <meta name="description" content="Hi! I'm Francois. I'm currently working at Asimmetric as a front-end developer within the JavaScript stack using React, Redux and Firebase. For the past few years I've worked as a full stack developer, primarily developing web apps using ASP.NET."/>
+            <meta name="keywords" content="Francois,Laubscher,JavaScript,JS,NodeJS,React,Redux,Cape Town,South Africa,Developer,Web developer"/>
             <title>francois laubscher - full stack developer</title>
             <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'></link>
             <link href="/css/site.min.css" rel="stylesheet"></link>
--- views/resume.jsx
+++ views/resume.jsx
@@ -5,7 +5,7 @@ const ProjectTemplate = ({proj}) =>
   <div className="project">
       <div className="title">{proj.title}</div>
       <div className="employer">
-          <a target="_blank" href={proj.employerlink}>{proj.employer}</a>
+          <a target="_blank" href={proj.employerlink}>{proj.employer}</a> ({proj.date})
       </div>
       <div className="description">
           <p>{proj.description}</p>
@@ -34,8 +34,8 @@ export class Resume extends React.Component {
             <head>
                 <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0" />
                 <meta name="author" content="Francois Laubscher" />
-                <meta name="description" content="Hi! I'm Francois. For the past few years I've worked as a full stack developer, primarily developing web apps using ASP.NET. I've recently started focusing my free time and career on developing within the JavaScript stack using tools such NodeJS, React and Redux." />
-                <meta name="keywords" content="Francois,Laubscher,JavaScript,JS,NodeJS,React,Cape Town,South Africa,Developer,Web developer" />
+                <meta name="description" content="Hi! I'm Francois. I'm currently working at Asimmetric as a front-end developer within the JavaScript stack using React, Redux and Firebase. For the past few years I've worked as a full stack developer, primarily developing web apps using ASP.NET." />
+                <meta name="keywords" content="Francois,Laubscher,JavaScript,JS,NodeJS,React,Redux,Cape Town,South Africa,Developer,Web developer" />
                 <title>francois laubscher - resume</title>
                 <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'></link>
                 <link href="/css/site.min.css" rel="stylesheet"></link>
@@ -47,8 +47,8 @@ export class Resume extends React.Component {
                             <img src="img/me-square_250.png" />
                         </div>
                         <h1>Hi! I'm Francois.</h1>
+                        <p>I'm currently working at Asimmetric as a front-end developer within the JavaScript stack using React, Redux and Firebase.</p>
                         <p>For the past few years I've worked as a full stack developer, primarily developing web apps using ASP.NET.</p>
-                        <p>I've recently started focusing my free time and career on developing within the JavaScript stack using tools such NodeJS, React and Redux.</p>
                         <p>Check out some of my open source projects on <a target="_blank" href="http://github.com/fjlaubscher">GitHub</a>.</p>
                         <div className="contact-footer">
                             <a target="_blank" href="http://www.linkedin.com/in/fjlaubscher">linkedin</a>
